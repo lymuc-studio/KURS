704182ec4f30a4359bf38f7a0895131c559ed49f
Leonid Ovsyannikov <lymuc.studio@gmail.com>
2023-03-18 15:11:24 +0300

    Improve namespace organization

--- kurs/src/Core/TypeName.hpp
+++ kurs/src/Core/TypeName.hpp
@@ -7,52 +7,49 @@
 #include "Core/FuncName.hpp"
 #include "Core/CompilerInfo.hpp"
 
-namespace kurs
+namespace kurs::detail
 {
-	namespace detail
+	template<typename T>
+	constexpr std::string_view getTypeNameGnu()
 	{
-		template<typename T>
-		constexpr std::string_view getTypeNameGnu()
-		{
-			constexpr std::string_view funcName = KURS_FUNCNAME;
+		constexpr std::string_view funcName = KURS_FUNCNAME;
 
-			constexpr size_t typeNameStart = funcName.find_first_of("=") + 2;
-			constexpr size_t typeNameEnd = funcName.find_first_of(";");
+		constexpr std::size_t typeNameStart = funcName.find_first_of("=") + 2;
+		constexpr std::size_t typeNameEnd = funcName.find_first_of(";");
 
-			return funcName.substr(typeNameStart, typeNameEnd - typeNameStart);
-		}
-
-		template<typename T>
-		constexpr std::string_view getTypeNameClang()
-		{
-			constexpr std::string_view funcName = KURS_FUNCNAME;
-
-			constexpr size_t typeNameStart = funcName.find_first_of("=") + 2;
-			constexpr size_t typeNameEnd = funcName.find_last_of("]");
-
-			return funcName.substr(typeNameStart, typeNameEnd - typeNameStart);
-		}
-
-		template<typename T>
-		constexpr std::string_view getTypeNameMsvc()
-		{
-			constexpr std::string_view funcSig = KURS_FUNCNAME;
-			constexpr std::string_view funcName = __func__;
-
-			constexpr std::string_view templateSigStart = funcSig.substr(
-				funcSig.find(funcName)
-			);
-
-			constexpr size_t typeNameStart = templateSigStart.find_first_of("<") + 1;
-			constexpr size_t typeNameEnd = templateSigStart.find_last_of(">");
-
-			return templateSigStart.substr(
-				typeNameStart, 
-				typeNameEnd - typeNameStart
-			);
-		}
+		return funcName.substr(typeNameStart, typeNameEnd - typeNameStart);
 	}
 
+	template<typename T>
+	constexpr std::string_view getTypeNameClang()
+	{
+		constexpr std::string_view funcName = KURS_FUNCNAME;
+
+		constexpr std::size_t typeNameStart = funcName.find_first_of("=") + 2;
+		constexpr std::size_t typeNameEnd = funcName.find_last_of("]");
+
+		return funcName.substr(typeNameStart, typeNameEnd - typeNameStart);
+	}
+
+	template<typename T>
+	constexpr std::string_view getTypeNameMsvc()
+	{
+		constexpr std::string_view funcSig = KURS_FUNCNAME;
+		constexpr std::string_view funcName = __func__;
+
+		constexpr std::string_view templateSigStart = funcSig.substr(
+			funcSig.find(funcName)
+		);
+
+		constexpr std::size_t typeNameStart = templateSigStart.find_first_of("<") + 1;
+		constexpr std::size_t typeNameEnd = templateSigStart.find_last_of(">");
+
+		return templateSigStart.substr(typeNameStart, typeNameEnd - typeNameStart);
+	}
+}
+	
+namespace kurs
+{
 #if KURS_COMPILER_ID == KURS_COMPILER_GCC
 	template<typename T>
 	constexpr std::string_view getTypeName()
